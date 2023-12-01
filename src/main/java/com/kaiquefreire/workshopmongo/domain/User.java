f08fd36f27758ece271f3aee954bdde9b9442944
Kaíque Freire <kaiquefreiresantos05@gmail.com>
2023-12-01 13:06:54 -0300

    entity user and rest working

---
+++ src/main/java/com/kaiquefreire/workshopmongo/domain/User.java
@@ -0,0 +1,72 @@
+package com.kaiquefreire.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String email;
+	private String name;
+	
+	// construtores
+	
+	public User () {}
+
+	public User(String id, String email, String name) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.name = name;
+	}
+	
+	// getters e setters
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	// hashcode e equals
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
